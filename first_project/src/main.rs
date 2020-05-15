336bd17c00e9ec313ee88528b98f07f7c590577c
Jeremy Frank <jfrank@summitinvest.tech>
2020-05-15 15:49:02 -0600

    more guessing game, pattern matching!

--- first_project/src/main.rs
+++ first_project/src/main.rs
@@ -1,25 +1,50 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
-fn get_secret_number() -> i32 {
+
+fn get_secret_number() -> u32 {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("secret is {}", secret_number);
     return secret_number;
 }
-fn guess_game() {
-    let sec_num = get_secret_number();
-    
-    println!("guess a number!");
-    println!("please input your guess");
 
-    
+fn get_guess() -> u32 {
     let mut guess = String::new();
 
+    println!("please input your guess");
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a positive integer!");
+
+    return guess;
+}
+
+fn compare_guess(guess:u32, sec_num:u32) -> String {
+    let result =
+        match guess.cmp(&sec_num) {
+            Ordering::Less => "too small",
+            Ordering::Greater => "too large",
+            Ordering::Equal => "you got it",
+        };
+    return result.to_string();
+}
+fn guess_game() {
+    
+    
+    println!("guess a number!");
+    
+    let guess = get_guess();
+    let sec_num = get_secret_number();
+    
     println!("you guessed {}", guess); 
+
+    let answer = compare_guess(guess, sec_num);
+    println!("{}", answer)
+
 }
 fn main() {
 //     let numbers = [1, 2, 5].iter();
