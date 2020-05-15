454b31a5990b1abe156ac51b11169a17e8e6d8c0
Jeremy Frank <jfrank@summitinvest.tech>
2020-05-15 12:59:41 -0600

    learning funcs

--- first_project/src/main.rs
+++ first_project/src/main.rs
@@ -3,12 +3,18 @@ extern crate rand;
 use std::io;
 use rand::Rng;
 
+fn get_secret_number() -> i32 {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("secret is {}", secret_number);
+    return secret_number;
+}
 fn guess_game() {
+    let sec_num = get_secret_number();
+    
     println!("guess a number!");
     println!("please input your guess");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("secret is {}", secret_number);
+    
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
@@ -16,10 +22,12 @@ fn guess_game() {
     println!("you guessed {}", guess); 
 }
 fn main() {
-    let numbers = [1, 2, 5].iter();
+//     let numbers = [1, 2, 5].iter();
 
-    let sum = numbers
-        .fold(0, |acc, next| acc+next);
+//     let sum = numbers
+//         .fold(0, |acc, next| acc+next);
     
-    println!("sum is {:#?}", sum);
+//     println!("sum is {:#?}", sum);
+
+    guess_game();
 }
