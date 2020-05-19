48b7a89dc7a4aa911e56fb587182a9fc25bb54a4
Jeremy Frank <jfrank@summitinvest.tech>
2020-05-19 09:02:23 -0600

    parse and filter

--- rest_stuff/src/main.rs
+++ rest_stuff/src/main.rs
@@ -1,47 +1,40 @@
 extern crate ureq;
 extern crate csv;
 
-use std::error::Error;
 use std::io;
-use std::process;
 
 use serde::Deserialize;
+use serde::de::DeserializeOwned;
 
-#[derive(Debug, Deserialize)]
+#[derive(Hash, Eq, PartialEq, Debug, Ord, PartialOrd, Deserialize)]
 struct CovidData {
     date : String,
     state : String,
     cases : u32,
     deaths : u32,
 }
-// fn getdata() -> Result<Vec<CovidData>, Box<dyn Error>> {
-//     let mut rdr = csv::Reader::from_reader(io::stdin());
-//     for result in rdr.deserialize() {
-//         // Notice that we need to provide a type hint for automatic
-//         // deserialization.
-//         let record: CovidData = result?;
-//         println!("{:?}", record);
-//         record
-//     }
-//     Ok(())
-// }
-fn main() {
+
+fn parse_csv<D: DeserializeOwned, R: io::Read>(rdr: R) -> csv::Result<Vec<D>> {
+    csv::Reader::from_reader(rdr).into_deserialize().collect()
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // sync post request of some json.
     let req = ureq::get("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
         .call()
         .into_string()
         .unwrap();
-    let mut csv = csv::Reader::from_reader(req.as_bytes());
+    let strreq = req.as_str();
+    let covid_data : Vec<CovidData> = parse_csv(strreq.as_bytes())?;
+    let filtered : Vec<CovidData> = covid_data
+        .into_iter()
+        .filter(|d| d.state == "Colorado")
+        .collect();
+
+    println!("{:?}", &filtered);
     
-    let results = {
-        for result in csv.deserialize(){
-            let record : CovidData = result.unwrap();
-            println!("{:?}", record);
-        };
-    };
-    println!("{:?}", results);
-    
+
+    Ok(())
     // .ok() tells if response is 200-299.
     
 }
